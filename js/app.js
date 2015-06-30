427b24ee00a8d8842ff5212c50a79c23d08f3bdc
gamaliellobato <gamaliellobato@gmail.com>
2015-06-30 13:27:45 -0500

    15. Api externa - Clima en el Mundo

--- js/app.js
+++ js/app.js
@@ -6,14 +6,25 @@ var app = angular.module('ejemplosApp',[ ]);
 
 app.controller('mainCtrl', ['$scope','$http', function($scope,$http){
   
-  //http://www.geoplugin.net/json.gp?jsoncallback=JSON_CALLBACK
-	$scope.geo = {};
+  //http://api.openweathermap.org/data/2.5/weather?lat=35&lon=139&callback=JSON_CALLBACK
+	$scope.clima = {};
 
-  	$http.jsonp('http://www.geoplugin.net/json.gp?jsoncallback=JSON_CALLBACK').success(function (data){
+	$http.jsonp('http://api.openweathermap.org/data/2.5/weather?lat=35&lon=139&callback=JSON_CALLBACK')
+		.success(function(data){
 
-  			$scope.geo = data;
+			$scope.clima = data;
 
-  	});
+		});
+
+	$scope.kelvinCelcius =  function (k){
+
+		c = k - 273.15;
+		c = Math.round( c*100) / 100;
+
+		return c;
+
+	};
+
 
 
 }]);
