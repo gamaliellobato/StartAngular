a07054ee6fe44d790dcd2288463cd0affb237937
gamaliellobato <gamaliellobato@gmail.com>
2015-07-05 15:22:36 -0500

    17. Tabla dinamica y filtro personalizado

--- js/alumnosCtrl.js
+++ js/alumnosCtrl.js
@@ -1,6 +1,41 @@
-app.controller('alumnosCtrl', ['$scope', function ($scope) {
+app.controller('alumnosCtrl', ['$scope','$http', function($scope,$http){
 	
 	$scope.setActive("mAlumnos");
 
+	$scope.alumnos = {};
+	$scope.posicion = 5;
 
-}])
+	$http.get('php/servicios/alumnos.listado.php').success(function(data){
+
+		$scope.alumnos = data;
+
+	});
+
+	$scope.siguientes = function (){
+
+		if($scope.alumnos.length > $scope.posicion){
+
+			$scope.posicion += 5;
+		};
+	};
+
+	$scope.anteriores = function (){
+
+		if($scope.posicion > 5){
+
+			$scope.posicion -= 5;
+		};
+	};
+
+}]);
+
+
+//Filtro personalizado de telefono
+
+app.filter('telefono',function (){
+
+	return function(numero){
+
+		return numero.substring(0,4) + "-" + numero.substring(4);
+	}
+});
--- js/inicioCtrl.js
+++ js/inicioCtrl.js
@@ -1,6 +1,6 @@
-app.controller('inicioCtrl', ['$scope', function ($scope) {
+app.controller('inicioCtrl', ['$scope', function($scope){
 	
 	$scope.setActive("mInicio");
 
 
-}])
+}]);
